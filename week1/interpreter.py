b35c92c3cc108b812f7a803112775b8b9eba4468
Jaspreet-SRAN7 <sranj667@gmail.com>
2025-07-16 20:53:36 +0530

    Update interpreter.py

--- week1/interpreter.py
+++ week1/interpreter.py
@@ -1,1 +1,25 @@
+def interpreter():
+    x,y,z=input("expression: ").split()
+    x=int(x)
+    z=int(z)
+    if(y=="+"):
+        result=float(x+z)
+        print(f"{result:.1f}")
+    elif(y=="-"):
+        result=float(x-z)
+        print(f"{result:.1f}")
+    elif(y=="/"):
+        if(z>0):
+            result=float(x/z)
+            print(f"{result:.1f}")
+        else:
+            print("z should be greater than zero")
+    elif(y=="*"):
+        result=float(x*z)
+        print(f"{result:.1f}")
+    else:
+        print("enter the operators(+,-,/,*)")
+interpreter()
+
+
 
