6e9f3d9e225fd47b6861561f76c753e5842fb96f
Jaspreet-SRAN7 <sranj667@gmail.com>
2025-07-16 20:46:22 +0530

    Update deep.py

--- week1/deep.py
+++ week1/deep.py
@@ -1,1 +1,11 @@
+def main():
+    answer = input(
+        "what is the answer to the greatest question of life ,the universe and everything? ")
+    answer = answer.strip().lower()
+    if (answer == "42" or answer == "forty-two" or answer == "forty two"):
+        print("yes")
+    else:
+        print("No")
 
+
+main()
