bc2738a52a764c89100d20a422e0f2153a64a2b3
Jaspreet-SRAN7 <sranj667@gmail.com>
2025-07-16 20:50:09 +0530

    Update bank.py

--- week1/bank.py
+++ week1/bank.py
@@ -1,1 +1,10 @@
+def greeting():
+    greeting=input("greeting: ").strip().lower()
+    if(greeting.startswith("hello")):
+        print("$0")
+    elif(greeting.startswith("h")):
+        print("$20")
+    else:
+        print("$100")
+greeting()
 
